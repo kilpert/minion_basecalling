0c3a91af360a3512374d1145464c7602ce6fc0d3
Fabian Kilpert <fkilpert@gmail.com>
2024-05-21 11:06:11 +0200

    update run.sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-##snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --rerun-trigger mtime
-snakemake -j 32 --profile humgen # --rerun-triggers mtime
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete #--rerun-trigger mtime
+
 
