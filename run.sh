2285c0c1014ebf8d5f1e6948c822af7a63666c29
Fabian Kilpert <fkilpert@gmail.com>
2024-08-30 15:28:15 +0200

    add run004, run005, run006

--- run.sh
+++ run.sh
@@ -5,6 +5,9 @@
 
 ## results.2024-8
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run001.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run004.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run005.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run006.minion.config.yaml
 
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
 
