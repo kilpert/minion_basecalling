d8a33fe094146b94335bdd4043b1bdf9e362665e
Fabian Kilpert <fkilpert@gmail.com>
2024-08-30 15:06:56 +0200

    add run001

--- run.sh
+++ run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 
 ## snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete #--rerun-trigger mtime
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
+
+
+## results.2024-8
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run001.minion.config.yaml
+
+## snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
 
