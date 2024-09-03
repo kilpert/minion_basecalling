5c5728b86dd77ea0fa3f9cba9b4628c7998c453c
Fabian Kilpert <fkilpert@gmail.com>
2024-09-03 09:11:54 +0200

    update comments

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 ## snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete #--rerun-trigger mtime
 
 
-## results.2024-8
+## results.2024-8 (FAME3, 2nd paper)
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run001.minion.config.yaml
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run004.minion.config.yaml
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run005.minion.config.yaml
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run006.minion.config.yaml
 
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
+# snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
 
