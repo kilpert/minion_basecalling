5fb50587b6b3bf913e83f489cda557afa5eee8d3
Fabian Kilpert <fkilpert@gmail.com>
2024-09-03 09:17:22 +0200

    add comment

--- run.sh
+++ run.sh
@@ -9,5 +9,6 @@ snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --con
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run005.minion.config.yaml
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run006.minion.config.yaml
 
-# snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
+## DAB1
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
 
