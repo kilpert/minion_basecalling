3cd000f2205c9ee15b77b3d0bd38d76ef662aed9
Fabian Kilpert <fkilpert@gmail.com>
2024-09-11 10:09:45 +0200

    add cmd lines for runs 007, 010, 011, 014

--- run.sh
+++ run.sh
@@ -8,6 +8,10 @@ snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --con
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run004.minion.config.yaml
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run005.minion.config.yaml
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run006.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run007.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run010.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run011.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
 
 ## DAB1
 snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
