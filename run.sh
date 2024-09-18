214d5aed6b7cc08eadd8c996b892d025b6ad6142
Fabian Kilpert <fkilpert@gmail.com>
2024-09-18 13:46:03 +0200

    use --rerun-trigger mtime

--- run.sh
+++ run.sh
@@ -4,14 +4,14 @@
 
 
 ## results.FAME3-2 (FAME3, 2nd paper)
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run001.minion.config.yaml
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run004.minion.config.yaml
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run005.minion.config.yaml
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run006.minion.config.yaml
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run007.minion.config.yaml
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run010.minion.config.yaml
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run011.minion.config.yaml
-snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --rerun-trigger mtime --configfile config/run001.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --rerun-trigger mtime --configfile config/run004.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --rerun-trigger mtime --configfile config/run005.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --rerun-trigger mtime --configfile config/run006.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --rerun-trigger mtime --configfile config/run007.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --rerun-trigger mtime --configfile config/run010.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --rerun-trigger mtime --configfile config/run011.minion.config.yaml
+snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --rerun-trigger mtime --configfile config/run014.minion.config.yaml
 
 ## DAB1
 # snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/run014.minion.config.yaml
