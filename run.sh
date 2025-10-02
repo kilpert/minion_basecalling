29cce277ff9a5b8209d69787c50a4450411db6fb
Fabian Kilpert <fkilpert@gmail.com>
2025-10-02 11:37:41 +0200

    add run16_20251001_MARCHF6_FGF14_RNU2s

--- run.sh
+++ run.sh
@@ -20,9 +20,13 @@
 # snakemake --use-conda -p --rerun-incomplete --configfile config/runs/run007.config.yaml
 # snakemake --use-conda -p --rerun-incomplete --configfile config/runs/run010.config.yaml
 # snakemake --use-conda -p --rerun-incomplete --configfile config/runs/run011.config.yaml
-snakemake --use-conda -p --rerun-incomplete --configfile config/runs/run013.config.yaml
+# snakemake --use-conda -p --rerun-incomplete --configfile config/runs/run013.config.yaml
 # snakemake --use-conda -p --rerun-incomplete --configfile config/runs/run014.config.yaml
 # snakemake --use-conda -p --rerun-incomplete --configfile config/runs/run015.config.yaml
 
 ## DAB1
 # snakemake --cores --use-conda --conda-frontend mamba -p --rerun-incomplete --configfile config/runs/run014.config.yaml
+
+## 2025
+snakemake --use-conda -p --rerun-incomplete --configfile config/runs/run016.config.yaml # run16_20251001_MARCHF6_FGF14_RNU2s
+
